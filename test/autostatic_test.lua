ba32cdca5960da00b095dcc4f8819c2dbe16977d
Mizu <40831463+Rexxt@users.noreply.github.com>
2021-11-22 14:20:49 +0100

    Update autostatic_test.lua

--- test/autostatic_test.lua
+++ test/autostatic_test.lua
@@ -12,3 +12,13 @@ print(namespace.bar)
 print(namespace.baz)
 namespace.bar = 12 -- should throw an error because namespace is statically typed
 print(namespace.bar)
+
+--[[ Prevent automatic static typing on a specific table:
+local enable_static, loosen = require "autostatic"
+enable_static(_G, "soft")
+myTable = loosen {
+    -- dynamically typed table
+    x = 10
+}
+myTable.x = "10" -- should not throw an error because myTable has been loosened
+]]
